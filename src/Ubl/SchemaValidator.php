7bdb4bb86b0890d1152f3f4dc4d529bede84411f
giansalex <giansalex@gmail.com>
2018-01-25 18:03:20 -0500

    add schema validator + add xsd files

---
+++ src/Ubl/SchemaValidator.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrador
+ * Date: 25/01/2018
+ * Time: 05:26 PM.
+ */
+
+namespace Greenter\Ubl\v1;
+
+use Greenter\Ubl\SchemaValidatorInterface;
+
+/**
+ * Class Validator.
+ */
+class Validator implements SchemaValidatorInterface
+{
+    /**
+     * @var string
+     */
+    private $error;
+
+    /**
+     * @var string
+     */
+    private $version = '2.0';
+
+    /**
+     * @param string $version UBL Version '2.0' or '2.1'
+     */
+    public function setVersion($version)
+    {
+        $this->version = $version;
+    }
+
+    /**
+     * Get last message error or warning.
+     *
+     * @return string
+     */
+    public function getMessage()
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param \DOMDocument|string $value Xml content or DomDocument
+     *
+     * @return bool
+     */
+    public function validate($value)
+    {
+        if ($value instanceof \DOMDocument) {
+            $doc = $value;
+        } else {
+            $doc = new \DOMDocument();
+            @$doc->loadXML($value);
+        }
+
+        $filename = $this->getFilename($doc->documentElement->nodeName);
+        if (!file_exists($filename)) {
+            $this->error = 'Schema file not found';
+
+            return false;
+        }
+
+        $state = libxml_use_internal_errors(true);
+        $result = $doc->schemaValidate($filename);
+        $this->error = $this->getErrors();
+        libxml_use_internal_errors($state);
+
+        return $result;
+    }
+
+    private function getErrors() {
+        $message = '';
+        $errors = libxml_get_errors();
+        foreach ($errors as $error) {
+            $message .= $this->getError($error).PHP_EOL;
+        }
+
+        libxml_clear_errors();
+
+        return $message;
+    }
+
+    function getError($error)
+    {
+        $msg = "<br/>\n";
+        switch ($error->level) {
+            case LIBXML_ERR_WARNING:
+                $msg .= 'Alerta';
+                break;
+            case LIBXML_ERR_ERROR:
+                $msg .= 'Error';
+                break;
+            case LIBXML_ERR_FATAL:
+                $msg .= 'Error Fatal';
+                break;
+        }
+        $msg .= $error->code.': '.trim($error->message);
+
+        if ($error->file) {
+            $msg .= 'in '.$error->file;
+        }
+        $msg .= 'en la linea '.$error->line;
+
+        return $msg;
+    }
+
+    private function getFilename($rootName)
+    {
+        if ($this->version == '2.0') {
+            $name = $rootName == 'DespatchAdvice' ? 'UBL-DespatchAdvice-2.0' : 'UBLPE-'.$rootName.'-1.0';
+        } else {
+            $name = 'UBL-'.$rootName.'-2.1';
+        }
+
+        $path = __DIR__.'/../../xsd/'.$this->version.'/'.$name.'.xsd';
+
+        return $path;
+    }
+}
---
+++ src/Ubl/SchemaValidatorInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrador
+ * Date: 25/01/2018
+ * Time: 05:23 PM
+ */
+
+namespace Greenter\Ubl;
+
+
+class SchemaValidatorInterface
+{
+
+}
